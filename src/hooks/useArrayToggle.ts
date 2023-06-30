e8dc88bda4feea2f2298820a0841f83582c7c3a0
alex-kurkov <alexkourkov@yandex.ru>
2023-06-30 11:55:42 +0500

    fix: improve arg checking in useArrayToggle hook

--- src/hooks/useArrayToggle.ts
+++ src/hooks/useArrayToggle.ts
@@ -1,25 +1,39 @@
 import { useState } from 'react';
 
+function makeInitialArray<T>(arr?: T[]): T[] {
+  if (Array.isArray(arr) && arr.length) {
+    return arr;
+  }
+  if (!!arr && !Array.isArray(arr)) {
+    console.error(
+      `переданный в хук аргумент ${arr} должен быть либо массивом, либо отсутствовать. Будут применены дефолтные значения: [true, false]`
+    );
+  }
+  return [true, false] as T[];
+}
+
 export function useArrayToggle<T extends string | boolean>(
-  array: T[] = [true, false] as T[]
+  array?: T[]
 ): [T, (...args: T[]) => void] {
+  const dataArray = makeInitialArray<T>(array);
+
   const [index, setIndex] = useState(0);
 
   const toggle = (...args: T[]) => {
     if (args.length === 0) {
-      setIndex((p) => (p + 1 === array.length ? 0 : p + 1));
+      setIndex((p) => (p + 1 === dataArray.length ? 0 : p + 1));
       return;
     }
 
-    const nextIndex = array.indexOf(args[0]);
+    const nextIndex = dataArray.indexOf(args[0]);
     if (nextIndex === -1) {
       console.error(
-        `the value of "${args[0]}" is not present in array: ${array}`
+        `the value of "${args[0]}" is not present in array: ${dataArray}`
       );
     } else {
       setIndex(nextIndex);
     }
   };
 
-  return [array[index], toggle];
+  return [dataArray[index], toggle];
 }
